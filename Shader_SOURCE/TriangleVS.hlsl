6276f9bc3be8e21c28c840125ad9e2c74cb6a522
o0ojenny <93306648+o0ojenny@users.noreply.github.com>
2023-06-07 04:46:46 +0900

    2023.06.06

--- Shader_SOURCE/TriangleVS.hlsl
+++ Shader_SOURCE/TriangleVS.hlsl
@@ -11,11 +11,18 @@ struct VSOut
     float4 Color : COLOR;
 };
 
+cbuffer Transform : register(b0)
+{
+    float4 Pos;
+}
+
 VSOut main(VSIn In)
 {
     VSOut Out = (VSOut)0.0f;
     
     Out.Pos = float4(In.Pos, 1.0f);
+    //Out.Pos.x += 0.4f; //inputPosX; -> 상수버퍼를 통해서 이 데이터가 넘어와야한다.
+    Out.Pos.x += Pos.x;
     Out.Color = In.Color;
     
     return Out;
